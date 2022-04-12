7aef232e9379daf47231b01529ab7ccef6f69b0c
lifeoflikhon <mehedi.hasansjs@gmail.com>
2022-04-12 17:08:47 +0600

    feat: made routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'task',
+    loadChildren: () => import('./../pages/task-detail/task-detail.module').then(m => m.TaskDetailModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./../pages/canvas/canvas.module').then(m => m.CanvasModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
