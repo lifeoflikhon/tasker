d83d1d563cb2d3149a4b719ba1e00ad0b325790f
lifeoflikhon <mehedi.hasansjs@gmail.com>
2022-04-13 15:26:01 +0600

    Revert "feat: made routes"
    This reverts commit 7aef232e9379daf47231b01529ab7ccef6f69b0c.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'task',
-    loadChildren: () => import('./../pages/task-detail/task-detail.module').then(m => m.TaskDetailModule)
-  },
-  {
-    path: '',
-    loadChildren: () => import('./../pages/canvas/canvas.module').then(m => m.CanvasModule)
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
